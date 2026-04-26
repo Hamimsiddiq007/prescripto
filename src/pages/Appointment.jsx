8bb7847c164611f842505d1f5922e01f4f32a9e1
Hamim <hamimsiddiq915@gmail.com>
2026-04-26 23:29:48 +0600

    feat: appointment page

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext';
 
 const Appointment = () => {
-  return (
+
+  const {docId} = useParams();
+  const {doctors} = useContext(AppContext)
+  const [docInfo, setDocInfo] = useState(null);
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find(doc => doc._id === docId);
+    setDocInfo(docInfo);
+    console.log(docInfo);
+    
+    
+  }
+
+  useEffect(() => {
+    fetchDocInfo();
+  }, [docId, doctors])
+
+  return docInfo && (
     <div>
+      {/* Doctors Details */}
+      <div className="">
+        <div className="">
+          <img src={docInfo?.image} alt={docInfo?.name} />
+        </div>
+      </div>
       
     </div>
   )
